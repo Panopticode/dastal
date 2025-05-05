6fb9bca14a8ce82892fb0830976263473b622bfd
Gabriele Santi <neonoetica@gmail.com>
2025-05-05 20:19:29 +0100

    Add printer utilit

---
+++ src/main/java/com/panopticode/utils/Printer.java
@@ -0,0 +1,104 @@
+package com.panopticode.utils;
+
+public final class Printer {
+    private Printer() {
+        throw new UnsupportedOperationException("Cannot instantiate utility class");
+    }
+
+    public static void printTable(boolean[][] table, boolean justifyRight) {
+        printTable(table, justifyRight, null, null);
+    }
+
+    public static void printTable(boolean[][] table, boolean justifyRight, String[] headers, String[] rowHeaders) {
+        var objTable = new String[table.length][table[0].length];
+
+        for (var row = 0; row < table.length; ++row) {
+            boolean[] r = table[row];
+            for (var col = 0; col < r.length; ++col) {
+                objTable[row][col] = r[col] ? "1" : "0";
+            }
+        }
+
+        printTable(objTable, justifyRight, headers, rowHeaders);
+    }
+
+    public static <T> void printTable(T[][] table, boolean justifyRight) {
+        printTable(table, justifyRight, null, null);
+    }
+
+    public static <T> void printTable(T[][] table, boolean justifyRight, String[] headers, String[] rowHeaders) {
+        if (table.length == 0 || table[0].length == 0) {
+            System.out.println("<Empty table>");
+            return;
+        }
+
+        int[] columnWidths = new int[table[0].length];
+        for (var i = 0; i < table[0].length; ++i) {
+            for (Object[] row : table) {
+                if (row[i] != null) {
+                    columnWidths[i] = Math.max(columnWidths[i], row[i].toString().length());
+                }
+            }
+        }
+
+        if (headers != null) {
+            for (var i = 0; i < headers.length; ++i) {
+                columnWidths[i] = Math.max(columnWidths[i], headers[i].length());
+            }
+        }
+
+        int rowHeaderWidth = 0;
+        if (rowHeaders != null) {
+            for (var rowHeader : rowHeaders) {
+                if (rowHeader != null) {
+                    rowHeaderWidth = Math.max(rowHeaderWidth, rowHeader.length());
+                }
+            }
+        }
+
+        var horizontalLine = new StringBuilder("+");
+        if (rowHeaders != null) {
+            horizontalLine.append("-".repeat(rowHeaderWidth + 2)).append("+");
+        }
+        for (int width : columnWidths) {
+            horizontalLine.append("-".repeat(width + 2)).append("+");
+        }
+
+        if (headers != null) {
+            System.out.println(horizontalLine);
+            if (rowHeaders != null) {
+                System.out.printf("| %" + rowHeaderWidth + "s |", "");
+            }
+            for (var i = 0; i < headers.length; ++i) {
+                if (justifyRight) {
+                    System.out.printf(" %" + columnWidths[i] + "s |", headers[i]);
+                } else {
+                    System.out.printf(" %-"+ columnWidths[i] + "s |", headers[i]);
+                }
+            }
+            System.out.println();
+        }
+
+        for (var row = 0; row < table.length; ++row) {
+            System.out.println(horizontalLine);
+            if (rowHeaders != null) {
+                String rowHeader = rowHeaders[row] != null ? rowHeaders[row] : "<null>";
+                System.out.printf("| %" + rowHeaderWidth + "s |", rowHeader);
+            }
+            else {
+                System.out.print("|");
+            }
+            var r = table[row];
+            for (var col = 0; col < r.length; ++col) {
+                var value = r[col] != null ? r[col].toString() : "<null>";
+                if (justifyRight) {
+                    System.out.printf(" %" + columnWidths[col] + "s |", value);
+                } else {
+                    System.out.printf(" %-"+ columnWidths[col] + "s |", value);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println(horizontalLine);
+    }
+}
---
+++ src/test/java/com/panopticode/utils/PrinterTest.java
@@ -0,0 +1,93 @@
+package com.panopticode.utils;
+
+import org.testng.annotations.Test;
+
+public class PrinterTest {
+    @Test
+    public void printTableObjects() {
+        var table = generateRandomStringTable(5, 3);
+
+        Printer.printTable(table, false);
+    }
+
+    @Test
+    public void printTableBooleanTypes() {
+        var table = generateRandomBooleanTable(5, 3);
+
+        Printer.printTable(table, false);
+    }
+
+    @Test
+    public void printTableBooleanTypesWithHeaders() {
+        var table = generateRandomBooleanTable(5, 3);
+        var headers = new String[]{
+                "A", "B", "C"
+        };
+        var rowHeaders = new String[]{
+                "1", "2", "3", "4", "5"
+        };
+
+        Printer.printTable(table, false, headers, rowHeaders);
+    }
+
+    @Test
+    public void printTableWithHeaders() {
+        var table = generateRandomStringTable(5, 3);
+        var headers = new String[]{
+                "A", "B", "C"
+        };
+        var rowHeaders = new String[]{
+                "1", "2", "3", "4", "5"
+        };
+
+        Printer.printTable(table, false, headers, rowHeaders);
+    }
+
+    @Test
+    public void printTableWithHeadersVariableLengths() {
+        var table = new String[][]{
+                {generateRandomString(3), generateRandomString(2), generateRandomString(1)},
+                {generateRandomString(4), generateRandomString(2), generateRandomString(6)},
+                {generateRandomString(5), generateRandomString(3), generateRandomString(8)},
+                {generateRandomString(1), generateRandomString(2), generateRandomString(5)},
+                {generateRandomString(2), generateRandomString(3), generateRandomString(4)},
+        };
+        var headers = new String[]{
+                "A", "B", "C"
+        };
+        var rowHeaders = new String[]{
+                "1", "2", "3", "4", "5"
+        };
+
+        Printer.printTable(table, false, headers, rowHeaders);
+    }
+
+    private static String[][] generateRandomStringTable(int rows, int cols) {
+        String[][] table = new String[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                table[i][j] = generateRandomString(5);
+            }
+        }
+        return table;
+    }
+
+    private static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomChar = (int) (Math.random() * 26) + 'a';
+            sb.append((char) randomChar);
+        }
+        return sb.toString();
+    }
+
+    private static boolean[][] generateRandomBooleanTable(int rows, int cols) {
+        boolean[][] table = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                table[i][j] = Math.random() < 0.5;
+            }
+        }
+        return table;
+    }
+}
